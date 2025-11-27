962e9fc8dc9d7166275c06099357bdf9f967cbe9
Lendrit11 <lendritl122@gmail.com>
2025-11-27 20:20:46 +0100

    feat(Backend): add RoleConfiguration entity configuration

---
+++ SmartSpaceManager/SmartSpaceManager.DAL/Configurations/RoleConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmartSpaceManager.Domain.model;
+
+namespace SmartSpaceManager.DAL.Configurations
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.ToTable("Roles");
+
+            builder.HasKey(r => r.Id);
+
+            builder.Property(r => r.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+    }
+}
