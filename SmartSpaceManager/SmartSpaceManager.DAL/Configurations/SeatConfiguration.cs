db959b18e6217084111574ed3d8f842fc91ffaa2
Lendrit11 <lendritl122@gmail.com>
2025-11-27 20:21:19 +0100

    feat(Backend): add SeatConfiguration entity configuration

---
+++ SmartSpaceManager/SmartSpaceManager.DAL/Configurations/SeatConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmartSpaceManager.Domain.model;
+
+namespace SmartSpaceManager.DAL.Configurations
+{
+    public class SeatConfiguration : IEntityTypeConfiguration<Seat>
+    {
+        public void Configure(EntityTypeBuilder<Seat> builder)
+        {
+            builder.ToTable("Seats");
+
+            builder.HasKey(s => s.Id);
+
+            builder.Property(s => s.Name)
+                .HasMaxLength(50);
+
+            builder.Property(s => s.PositionX).HasDefaultValue(0);
+            builder.Property(s => s.PositionY).HasDefaultValue(0);
+
+            builder.HasOne(s => s.Desk)
+                .WithMany(d => d.Seats)
+                .HasForeignKey(s => s.DeskId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
