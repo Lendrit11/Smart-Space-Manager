855123d02feb113ef95cff1a3b22dc880af1a73f
Lendrit11 <lendritl122@gmail.com>
2025-11-27 20:22:01 +0100

    feat(Backend): add TimeSlotConfiguration entity configuration

---
+++ SmartSpaceManager/SmartSpaceManager.DAL/Configurations/TimeSlotConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmartSpaceManager.Domain.model;
+
+namespace SmartSpaceManager.DAL.Configurations
+{
+    public class TimeSlotConfiguration : IEntityTypeConfiguration<TimeSlot>
+    {
+        public void Configure(EntityTypeBuilder<TimeSlot> builder)
+        {
+            builder.ToTable("TimeSlots");
+
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.StartTime).IsRequired();
+            builder.Property(t => t.EndTime).IsRequired();
+            builder.Property(t => t.Description).HasMaxLength(100);
+        }
+    }
+}
