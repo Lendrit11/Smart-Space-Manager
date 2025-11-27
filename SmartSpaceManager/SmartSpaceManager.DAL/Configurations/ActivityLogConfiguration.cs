62f23882c4973cdf15c7d51f980e667d5cf22e36
Lendrit11 <lendritl122@gmail.com>
2025-11-27 20:16:52 +0100

    feat(Backend): add ActivityLog entity configuration

---
+++ SmartSpaceManager/SmartSpaceManager.DAL/Configurations/ActivityLogConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmartSpaceManager.Domain.model;
+
+namespace SmartSpaceManager.DAL.Configurations
+{
+    public class ActivityLogConfiguration : IEntityTypeConfiguration<ActivityLog>
+    {
+        public void Configure(EntityTypeBuilder<ActivityLog> builder)
+        {
+            builder.ToTable("ActivityLogs");
+
+            builder.HasKey(a => a.Id);
+
+            builder.Property(a => a.Action)
+                .HasMaxLength(255);
+
+            builder.Property(a => a.CreatedAt)
+                .HasDefaultValueSql("GETDATE()");
+
+            builder.HasOne(a => a.User)
+                .WithMany(u => u.ActivityLogs)
+                .HasForeignKey(a => a.UserId)
+                .OnDelete(DeleteBehavior.SetNull);
+        }
+    }
+}
