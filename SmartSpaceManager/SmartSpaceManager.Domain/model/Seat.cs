cc53680971eb229894af2d848456476ce789a521
Jahirq1 <jahirqoqaj8@gmail.com>
2025-11-27 20:35:41 +0100

    feat: have created the Seat(Model)

---
+++ SmartSpaceManager/SmartSpaceManager.Domain/model/Seat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSpaceManager.Domain.model
+{
+    public class Seat
+    {
+        public int Id { get; set; }
+        public int DeskId { get; set; }
+        public string? Name { get; set; }
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+
+        public Desk Desk { get; set; }
+    }
+}
