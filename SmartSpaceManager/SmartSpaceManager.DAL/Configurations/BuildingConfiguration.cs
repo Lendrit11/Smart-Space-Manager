9dd86d34f44e82831512f45b87654b6c07bdcdb0
Lendrit11 <lendritl122@gmail.com>
2025-11-27 20:18:11 +0100

    feat(Backend): add BuildingConfiguration entity configuration

---
+++ SmartSpaceManager/SmartSpaceManager.DAL/Configurations/BuildingConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmartSpaceManager.Domain.model;
+
+namespace SmartSpaceManager.DAL.Configurations
+{
+    public class BuildingConfiguration : IEntityTypeConfiguration<Building>
+    {
+        public void Configure(EntityTypeBuilder<Building> builder)
+        {
+            builder.ToTable("Buildings");
+
+            builder.HasKey(b => b.Id);
+
+            builder.Property(b => b.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(b => b.Address)
+                .HasMaxLength(255);
+
+            builder.Property(b => b.Description)
+                .HasColumnType("TEXT");
+
+            builder.HasMany(b => b.Floors)
+                .WithOne(f => f.Building)
+                .HasForeignKey(f => f.BuildingId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
+
