887bd63252971ce02ca788f0de1ae4a96aa195ee
Jahirq1 <jahirqoqaj8@gmail.com>
2025-11-27 21:15:50 +0100

    fix: have rewrite  Floor(Model)

--- SmartSpaceManager/SmartSpaceManager.Domain/model/Floor.cs
+++ SmartSpaceManager/SmartSpaceManager.Domain/model/Floor.cs
@@ -12,6 +12,9 @@ namespace SmartSpaceManager.Domain.model
         public int BuildingId { get; set; }
         public int FloorNumber { get; set; }
         public string? Description { get; set; }
+        public int Width { get; set; } = 1000;
+        public int Height { get; set; } = 1000;
+        public float Scale { get; set; } = 1f;
 
         public Building? Building { get; set; }
         public ICollection<Desk> Desks { get; set; }
