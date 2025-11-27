d57c3090db91af532fd977237bec1525916eb9aa
Jahirq1 <jahirqoqaj8@gmail.com>
2025-11-27 20:36:09 +0100

    feat: have created the Desk(Model)

---
+++ SmartSpaceManager/SmartSpaceManager.Domain/model/Desk.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSpaceManager.Domain.model
+{
+    public class Desk
+    {
+        public int Id { get; set; }
+        public int FloorId { get; set; }
+        public string Name { get; set; }
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Shape { get; set; }
+
+        public Floor Floor { get; set; }
+        public ICollection<Seat> Seats { get; set; }
+
+    }
+}
