03f6496d33213e3dc2bcb077f15179e9a93f159c
Lendrit11 <lendritl122@gmail.com>
2025-12-16 23:40:49 +0100

    feat(desk): add Desk and Seat DTOs

---
+++ SmartSpaceManager/SmartSpaceManager.Service/Dtos/DeskDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSpaceManager.Service.Dtos
+{
+    public class DeskDto
+    {
+        public int Id { get; set; }
+        public int? FloorId { get; set; }
+        public string Name { get; set; }
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Shape { get; set; }
+
+        public ICollection<SeatDto> Seats { get; set; } = new List<SeatDto>();
+    }
+
+    public class SeatDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+        public int? ReservationId { get; set; } // ⚡ nullable
+    }
+
+}
---
+++ SmartSpaceManager/SmartSpaceManager.Service/Dtos/SeatsDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSpaceManager.Service.Dtos
+{
+    public class SeatsDto
+    {
+        public int Id { get; set; }
+        public int DeskId { get; set; }
+        public string? Name { get; set; }
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+        public List<int> ReservationIds { get; set; }
+    }
+}
