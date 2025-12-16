e5ecb907fadd11fa17e7892aa2c4c4034620d365
Jahirq1 <jahirqoqaj8@gmail.com>
2025-12-16 15:05:31 +0100

    feat: created token generator

---
+++ SmartSpaceManager/SmartSpaceManager.Service/Interface/ITokenService.cs
@@ -0,0 +1,15 @@
+﻿using SmartSpaceManager.Domain.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSpaceManager.Service.Interface
+{
+    public interface ITokenService
+    {
+        public string GenerateAccessToken(User user);
+        public string GenerateRefreshToken();
+    }
+}
---
+++ SmartSpaceManager/SmartSpaceManager.Service/Services/TokenService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using SmartSpaceManager.Domain.model;
+using SmartSpaceManager.Service.Interface;
+
+namespace SmartSpaceManager.Service.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration _configuration;
+        public TokenService(IConfiguration config) => _configuration = config;
+
+        public string GenerateAccessToken(User user)
+        {
+            var claims = new List<Claim>
+{
+    new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+    new Claim(ClaimTypes.Email, user.Email),
+    new Claim(ClaimTypes.Name, user.FirstName),
+    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+};
+
+            // shto rolet
+            foreach (var userRole in user.UserRoles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, userRole.Role.Name));
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds
+            );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public string GenerateRefreshToken()
+        {
+              var randomNumber = new byte[64];
+              using var rng =RandomNumberGenerator.Create();
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
+        }
+    }
+}
