5afb590a2c0cca5faa330da84cd0e842707f170c
Jahirq1 <jahirqoqaj8@gmail.com>
2025-12-16 15:01:30 +0100

    fix: adding user & role module references

--- SmartSpaceManager/SmartSpaceManager.API/Program.cs
+++ SmartSpaceManager/SmartSpaceManager.API/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using SmartSpaceManager.DAL;
+using SmartSpaceManager.DAL.Implementations;
+using SmartSpaceManager.DAL.Interfaces;
+using SmartSpaceManager.Service.Interface;
+using SmartSpaceManager.Service.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Read connection string from appsettings.json
@@ -9,7 +13,11 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<RoleService>();
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
 // Register DbContext with SQL Server provider
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(connectionString));
